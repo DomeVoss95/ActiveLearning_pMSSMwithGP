3888eccb767a61b0004df81235d039643ab5a546
dvoss <dominik.voss@cern.ch>
2025-01-31 16:13:24 +0100

    Real Data Script is now working in n_dim and also FullyTraining

---
+++ model/slurm/job_RealData.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# python /raven/u/dvoss/al_pmssmwithgp/model/GPmodelRealData.py
+
+## Set the paths and variables
+GP_MODEL_SCRIPT="/u/dvoss/al_pmssmwithgp/model/GPmodelRealData.py"
+ITERATIONS=50
+
+
+for i in $(seq 1 ${ITERATIONS}); do
+    echo "Starting iteration ${i}"
+
+    OUTPUT_DIR="/raven/u/dvoss/al_pmssmwithgp/model/plots/Iter${i}"
+    mkdir -p ${OUTPUT_DIR}
+
+    # Step 1: Train GP Model and get new points
+    GP_COMMAND=("python" "${GP_MODEL_SCRIPT}" "--iteration=${i}" "--output_dir=${OUTPUT_DIR}")
+    "${GP_COMMAND[@]}"
+    if [ $? -ne 0 ]; then
+        echo "Error during GP model script execution"
+        exit 1
+    fi
+
+done
+
+echo "All iterations completed."
